3d2ac0bbc6a7a17f4a95d82514d80df255398af8
Joshua Cotton <jcotton42@outlook.com>
2018-01-06 22:25:43 -0600

    Fix exceptions in message parsing test

--- Tests/IRCMessageTests.cs
+++ Tests/IRCMessageTests.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.IO;
+using System.Linq;
 using ReactiveIRC;
 using Xunit;
 using YamlDotNet.Serialization;
@@ -23,8 +24,9 @@ namespace Tests {
             var atoms = data.Atoms;
             Assert.Equal(atoms.Source ?? "", message.Source);
             Assert.Equal(atoms.Verb.ToUpperInvariant(), message.Verb);
-            Assert.Equal(atoms.Params ?? (IEnumerable<string>)ImmutableArray<string>.Empty, message.Parameters);
-            Assert.Equal(atoms.Tags ?? (IDictionary<string, string>)ImmutableDictionary<string, string>.Empty, message.Tags, new NullFriendlyDictionaryComparer());
+            Assert.Equal(atoms.Params ?? (IEnumerable<string>)ImmutableArray<string>.Empty,
+                message.Parameters);
+            Assert.Equal(atoms.Tags, message.Tags, new NullFriendlyDictionaryComparer());
         }
 
         public static IEnumerable<object[]> GetSplitData() {
@@ -34,12 +36,22 @@ namespace Tests {
         }
     }
 
-    public class NullFriendlyDictionaryComparer : IEqualityComparer<KeyValuePair<string, string>> {
-        public bool Equals(KeyValuePair<string, string> x, KeyValuePair<string, string> y) {
-            return x.Key.Equals(y.Key) && x.Value.Equals(y.Value ?? "");
+    public class NullFriendlyDictionaryComparer : IEqualityComparer<IDictionary<string, string>> {
+        static InnerNullFriendlyDictionaryComparer comparer = new InnerNullFriendlyDictionaryComparer();
+
+        public bool Equals(IDictionary<string, string> x, IDictionary<string, string> y) {
+            return !(x ?? ImmutableDictionary<string, string>.Empty).Except(y, comparer).Any();
         }
 
-        public int GetHashCode(KeyValuePair<string, string> obj) => obj.GetHashCode();
+        public int GetHashCode(IDictionary<string, string> obj) => obj.GetHashCode();
+
+        class InnerNullFriendlyDictionaryComparer : IEqualityComparer<KeyValuePair<string, string>> {
+            public bool Equals(KeyValuePair<string, string> x, KeyValuePair<string, string> y) {
+                return x.Key.Equals(y.Key) && (x.Value ?? "").Equals(y.Value ?? "");
+            }
+
+            public int GetHashCode(KeyValuePair<string, string> obj) => obj.GetHashCode();
+        }
     }
 
     public class Root {
