4f6d3106c405567f1a77414e9225b4bc8e594f75
Joshua Cotton <jcotton42@outlook.com>
2018-01-06 23:52:54 -0600

    Add test for IRCMessage.ToString

--- Tests/IRCMessageTests.cs
+++ Tests/IRCMessageTests.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.IO;
@@ -10,11 +11,18 @@ using YamlDotNet.Serialization.NamingConventions;
 namespace Tests {
     public class IRCMessageTests {
         private static MessageTest[] MessageSplitData;
+        private static MessageTest[] MessageJoinData;
 
         static IRCMessageTests() {
-            var input = new StreamReader("irc-parser-tests/tests/msg-split.yaml");
             var deserializer = new DeserializerBuilder().WithNamingConvention(new CamelCaseNamingConvention()).Build();
+
+            var input = new StreamReader("irc-parser-tests/tests/msg-split.yaml");
             MessageSplitData = deserializer.Deserialize<Root>(input).Tests;
+            input.Dispose();
+
+            input = new StreamReader("irc-parser-tests/tests/msg-join.yaml");
+            MessageJoinData = deserializer.Deserialize<Root>(input).Tests;
+            input.Dispose();
         }
 
         [Theory]
@@ -29,8 +37,28 @@ namespace Tests {
             Assert.Equal(atoms.Tags, message.Tags, new NullFriendlyDictionaryComparer());
         }
 
+        [Theory]
+        [MemberData(nameof(GetJoinData))]
+        public void TestMessageToString(MessageTest data) {
+            var atoms = data.Atoms;
+            var message = new IRCMessage(
+                atoms.Tags?.ToImmutableDictionary() ?? ImmutableDictionary<string, string>.Empty,
+                atoms.Source ?? "",
+                atoms.Verb,
+                atoms.Params?.ToImmutableArray() ?? ImmutableArray<string>.Empty)
+                .ToString();
+
+            Assert.Contains(message, data.Matches, StringComparer.OrdinalIgnoreCase);
+        }
+
         public static IEnumerable<object[]> GetSplitData() {
             foreach(var d in MessageSplitData) {
+                yield return new object[] { d };
+            }
+        }
+
+        public static IEnumerable<object[]> GetJoinData() {
+            foreach(var d in MessageJoinData) {
                 yield return new object[] { d };
             }
         }
@@ -61,6 +89,8 @@ namespace Tests {
     public class MessageTest {
         public string Input { get; set; }
         public MessageAtoms Atoms { get; set; }
+        public string[] Matches { get; set; }
+        public string Desc { get; set; }
     }
 
     public class MessageAtoms {
