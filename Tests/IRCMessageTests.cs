d7e12455444a96514eb004cc47e278a399be9da7
Joshua Cotton <jcotton42@outlook.com>
2018-01-06 22:01:55 -0600

    Add message parsing test

--- Tests/IRCMessageTests.cs
+++ Tests/IRCMessageTests.cs
@@ -1,11 +1,60 @@
-using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.IO;
+using ReactiveIRC;
 using Xunit;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
 
 namespace Tests {
     public class IRCMessageTests {
-        [Fact]
-        public void Test1() {
+        private static MessageTest[] MessageSplitData;
 
+        static IRCMessageTests() {
+            var input = new StreamReader("irc-parser-tests/tests/msg-split.yaml");
+            var deserializer = new DeserializerBuilder().WithNamingConvention(new CamelCaseNamingConvention()).Build();
+            MessageSplitData = deserializer.Deserialize<Root>(input).Tests;
         }
+
+        [Theory]
+        [MemberData(nameof(GetSplitData))]
+        public void TestMessageParsing(MessageTest data) {
+            var message = IRCMessage.Parse(data.Input);
+            var atoms = data.Atoms;
+            Assert.Equal(atoms.Source ?? "", message.Source);
+            Assert.Equal(atoms.Verb.ToUpperInvariant(), message.Verb);
+            Assert.Equal(atoms.Params ?? (IEnumerable<string>)ImmutableArray<string>.Empty, message.Parameters);
+            Assert.Equal(atoms.Tags ?? (IDictionary<string, string>)ImmutableDictionary<string, string>.Empty, message.Tags, new NullFriendlyDictionaryComparer());
+        }
+
+        public static IEnumerable<object[]> GetSplitData() {
+            foreach(var d in MessageSplitData) {
+                yield return new object[] { d };
+            }
+        }
+    }
+
+    public class NullFriendlyDictionaryComparer : IEqualityComparer<KeyValuePair<string, string>> {
+        public bool Equals(KeyValuePair<string, string> x, KeyValuePair<string, string> y) {
+            return x.Key.Equals(y.Key) && x.Value.Equals(y.Value ?? "");
+        }
+
+        public int GetHashCode(KeyValuePair<string, string> obj) => obj.GetHashCode();
+    }
+
+    public class Root {
+        public MessageTest[] Tests { get; set; }
+    }
+
+    public class MessageTest {
+        public string Input { get; set; }
+        public MessageAtoms Atoms { get; set; }
+    }
+
+    public class MessageAtoms {
+        public string Source { get; set; }
+        public string Verb { get; set; }
+        public string[] Params { get; set; }
+        public Dictionary<string, string> Tags { get; set; }
     }
 }
