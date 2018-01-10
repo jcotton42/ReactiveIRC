d4c61cd42db189eed695c0cace30344254aa8d72
Joshua Cotton <jcotton42@outlook.com>
2018-01-09 22:46:18 -0600

    Add skeleton for client and handlers

---
+++ ReactiveIRC/Handlers/IIRCMessageHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ReactiveIRC.Handlers {
+    public interface IIRCMessageHandler {
+        void HandleIncoming(IRCClient client, IRCMessage message);
+        void HandleOutgoing(IRCClient client, IRCMessage message);
+    }
+
+    [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = true)]
+    public sealed class IRCMessageHandlerAttribute : Attribute {
+        public string Verb { get; }
+
+        public IRCMessageHandlerAttribute(string verb) {
+            Verb = verb ?? throw new ArgumentNullException(nameof(verb));
+        }
+    }
+}
--- ReactiveIRC/IRCClient.cs
+++ ReactiveIRC/IRCClient.cs
@@ -1,6 +1,66 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Reactive;
+using System.Reflection;
+using System.Threading.Tasks;
+using ReactiveIRC.Handlers;
 
 namespace ReactiveIRC {
     public class IRCClient {
+        private readonly IObservable<IRCMessage> incoming;
+        private readonly IObserver<IRCMessage> outgoing;
+        private static readonly Dictionary<string, List<IIRCMessageHandler>> messageHandlers;
+
+        static IRCClient() {
+            messageHandlers = new Dictionary<string, List<IIRCMessageHandler>>();
+            var types = AppDomain.CurrentDomain.GetAssemblies().SelectMany(a => a.GetExportedTypes())
+                .Where(typeof(IIRCMessageHandler).IsAssignableFrom);
+            foreach(var type in types) {
+                var attributes = type.GetCustomAttributes<IRCMessageHandlerAttribute>();
+                if(attributes is null) {
+                    throw new InvalidOperationException("TODO: need to log here.");
+                }
+                var instance = (IIRCMessageHandler)Activator.CreateInstance(type);
+                foreach(var verb in attributes.Select(a => a.Verb)) {
+                    if(!messageHandlers.ContainsKey(verb)) {
+                        messageHandlers[verb] = new List<IIRCMessageHandler>();
+                    }
+                    messageHandlers[verb].Add(instance);
+                }
+            }
+        }
+
+        public IRCClient(IObservable<IRCMessage> incoming, IObserver<IRCMessage> outgoing) {
+            this.incoming = incoming ?? throw new ArgumentNullException(nameof(incoming));
+            this.outgoing = outgoing ?? throw new ArgumentNullException(nameof(outgoing));
+        }
+
+        public async Task RegisterAsync(string password, string nick, string username, string realname) {
+            SendMessage("CAP", "LS", "302");
+            if(!string.IsNullOrWhiteSpace(password)) {
+                SendMessage("PASS", password);
+            }
+            SendMessage("NICK", nick);
+            SendMessage("USER", username, realname);
+        }
+
+        public void SendMessage(string verb, params string[] parameters) {
+            SendMessage(IRCMessage.Create(verb, parameters));
+        }
+
+        public void SendMessage(ImmutableDictionary<string, string> tags, string verb, params string[] parameters) {
+            SendMessage(IRCMessage.Create(tags, verb, parameters));
+        }
+
+        public void SendMessage(IRCMessage message) {
+            if(messageHandlers.TryGetValue(message.Verb, out var handlers)) {
+                foreach(var handler in handlers) {
+                    handler.HandleOutgoing(this, message);
+                }
+            }
+            outgoing.OnNext(message);
+        }
     }
 }
